488724ff86e9dfd69be043c19ff8f5b8a2ef0b85
Thijs van der Heijden <hi@thijsheijden.nl>
2019-03-01 17:54:43 +0100

    Second commit.

--- wwdc19.playground/Contents.swift
+++ wwdc19.playground/Contents.swift
@@ -5,4 +5,5 @@ import SpriteKit
 let scene = MenuScene(size: GameVariables.sceneView.bounds.size)
 GameVariables.sceneView.presentScene(scene)
 
+// Initializing the menu scene when the playground starts
 PlaygroundSupport.PlaygroundPage.current.liveView = GameVariables.sceneView
